ae0e1d1b07fe7649055255fe11cf0ca4f26ebdfd
eight_y_88 <jadec.eight@gmail.com>
2022-03-12 15:13:04 +0900

    Initial Commit

---
+++ SaveClipboard/Program.cs
@@ -0,0 +1,50 @@
+using System.Text.Json;
+
+namespace SaveClipboard
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string fileName = Directory.GetCurrentDirectory() + "\\config.json";
+            Config C;
+            if (!File.Exists(fileName))
+            {
+                C = GetDefaultConfig();
+                string str = JsonSerializer.Serialize(
+                    C, new JsonSerializerOptions { WriteIndented = true }
+                );
+
+                File.WriteAllText(fileName, str);
+            } else
+            {
+                Config? temp = JsonSerializer.Deserialize<Config>(File.ReadAllText(fileName));
+                if (temp == null)
+                {
+                    temp = GetDefaultConfig();
+                }
+                C = temp;
+            }
+
+            SaveClipboard Instance = new(C.PathToSave);
+
+            Instance.save();
+        }
+
+        private static Config GetDefaultConfig()
+        {
+            return new() { PathToSave = Directory.GetCurrentDirectory() + "\\saves\\" };
+        }
+    }
+
+    public class Config 
+    { 
+        
+        public string PathToSave { get => _PathToSave; set => _PathToSave = value ?? Directory.GetCurrentDirectory() + "\\saves\\"; }
+        private string _PathToSave = Directory.GetCurrentDirectory() + "\\saves\\";
+    }
+}
---
+++ SaveClipboard/SaveClipboard.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Specialized;
+using System.Drawing.Imaging;
+
+namespace SaveClipboard
+{
+    internal class SaveClipboard
+    {
+        string PathToSave;
+        public SaveClipboard(string PathToSave)
+        {
+            this.PathToSave = PathToSave;
+        }
+
+        public void save()
+        {
+            if (!Directory.Exists(PathToSave))
+            {
+                Directory.CreateDirectory(PathToSave);
+            }
+
+            if (Clipboard.ContainsImage())
+            {
+                Image i = Clipboard.GetImage();
+                string name = DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss");
+
+                string fileName = PathToSave + name + "-%id%." + GetImageExtension(i);
+                int c = 1;
+                while (File.Exists(fileName.Replace("%id%", c.ToString().Equals("1") ? "" : c.ToString()))) c++;
+                i.Save(PathToSave);
+            }
+            else if (Clipboard.ContainsText())
+            {
+                string s = Clipboard.GetText();
+                string name = DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss");
+
+                string fileName = PathToSave + name + "-%id%.txt";
+                int c = 1;
+                while (File.Exists(fileName.Replace("%id%", c.ToString().Equals("1") ? "" : c.ToString()))) c++;
+
+                fileName = fileName.Replace("%id%", c.ToString().Equals("1") ? "" : c.ToString());
+
+                File.WriteAllText(fileName, s);
+            }
+        }
+
+        private static string GetImageExtension(Image image)
+        {
+            if (image.RawFormat.Equals(ImageFormat.Jpeg))
+                return "jpg";
+            if (image.RawFormat.Equals(ImageFormat.Bmp))
+                return "bmp";
+            if (image.RawFormat.Equals(ImageFormat.Png))
+                return "png";
+            if (image.RawFormat.Equals(ImageFormat.Emf))
+                return "emf";
+            if (image.RawFormat.Equals(ImageFormat.Exif))
+                return "exif";
+            if (image.RawFormat.Equals(ImageFormat.Gif))
+                return "gif";
+            if (image.RawFormat.Equals(ImageFormat.Icon))
+                return "ico";
+            if (image.RawFormat.Equals(ImageFormat.MemoryBmp))
+                return "bmp";
+            if (image.RawFormat.Equals(ImageFormat.Tiff))
+                return "tiff";
+            else
+                return "wmf";
+        }
+    }
+}
